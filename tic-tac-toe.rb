da822627c77e8e8e95e9aacec71800b6a25e9e93
FelipeEnne <felipeenne@gmail.com>
2019-11-25 13:44:19 -0300

    create a table

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,1 +1,25 @@
 # frozen_string_literal: true
+
+$table = [[1,1,1],[1,2,1],[1,2,1]]
+
+class PutsTable 
+
+  $table.each do |x|
+    x.each do |y|
+      if y == 1
+        print " O "
+      elsif y == 2
+        print " X "
+      else
+        print " "
+      end
+    end
+    puts " "
+  end
+
+end
+
+
+a = PutsTable.new
+
+a
