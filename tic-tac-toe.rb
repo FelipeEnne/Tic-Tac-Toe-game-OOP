17c5347955b7d21595c1b85fcbe6f274d3d9debb
300ms <ilhansnmz44@gmail.com>
2019-11-25 20:35:44 +0300

    move and setmove methods have been written

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,25 +1,60 @@
 # frozen_string_literal: true
 
-$table = [[1,1,1],[1,2,1],[1,2,1]]
 
-class PutsTable 
+#$table = [[1,1,0],[1,2,1],[1,2,1]]
 
-  $table.each do |x|
-    x.each do |y|
-      if y == 1
-        print " O "
-      elsif y == 2
-        print " X "
-      else
-        print " "
+
+class TicTacToe
+  def initialize
+    $table = [[0,0,0],[0,0,0],[0,0,0]]
+    putTable
+  end
+
+  def putTable
+    $table.each do |x|
+      x.each do |y|
+        if y == 1
+          print " O "
+        elsif y == 2
+          print " X "
+        else
+          print "_"
+        end
       end
+      puts " "
     end
-    puts " "
+  end
+
+  def move(player)
+    puts "#{player} coordinates:"
+    coordinates = gets.chomp
+    coordinates = coordinates.split("").map { |s| s.to_i }
+
+    setmove(coordinates, player)
+  end
+
+  def setmove(coordinates, player)
+    coorx = coordinates[0]
+    coory = coordinates[1]
+
+    if coorx<3 && coorx>=0 && coory<3 && coory>=0
+      if $table[coorx][coory] != 0
+        puts "Error: coordinates are occupied"
+        move(player)  ###### arguments
+      else
+        $table[coorx][coory] = player
+        putTable
+      end
+    elsif
+    puts "Error: coordinates are not between 1-3"
+      ################# Call the move again #############
+    end
+    move(player) ###### arguments
   end
 
 end
 
+a = TicTacToe.new
 
-a = PutsTable.new
 
-a
+a.move(1)
